1bc41409d2cc6c9963f98c029c28151cfecb21b9
Shaitaxx <y.kenta.x@icloud.com>
2022-05-01 04:39:33 +0900

    モデル,migrateファイルの設定

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2022_04_30_102651) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
