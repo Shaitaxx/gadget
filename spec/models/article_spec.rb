4cdbe086eba47bf5b8076f22ee14f441e397f37a
Shaitaxx <y.kenta.x@icloud.com>
2022-04-30 18:21:47 +0900

    単体テストコードの実装

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  before do
+    @article = FactoryBot.build(:article)
+  end
+
+  describe '商品出品機能' do
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@article).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it '画像を1枚つけないと保存できないこと' do
+        @article.image = nil
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'titleがないと保存できないこと' do
+        @article.title = ''
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'textがないと保存できないこと' do
+        @article.text = ''
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'priceがないと保存できないこと' do
+        @article.price = ''
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Price can't be blank")
+      end
+      # it 'userが存在しなければ投稿できないこと' do
+      # end
+    end
+  end
+end
