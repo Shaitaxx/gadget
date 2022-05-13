be8c7f070c8d0287555ec65367ff4b3bea9fa486
Shaitaxx <y.kenta.x@icloud.com>
2022-05-13 23:18:02 +0900

    アソシエーションの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  has_many :comments
 
   with_options presence: true do
     validates :title, :text, :image, :price
