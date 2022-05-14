9775efd44aa3067f07ab34c048335081bfbf59f5
Shaitaxx <y.kenta.x@icloud.com>
2022-05-15 02:48:02 +0900

    コントローラーの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to article_path(params[:article_id])
+      ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user}
     end
   end
 
