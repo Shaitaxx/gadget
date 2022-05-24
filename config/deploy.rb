329bee41acd5c135ed372bd7bac61587e8b26e34
Shaitaxx <y.kenta.x@icloud.com>
2022-05-25 00:47:40 +0900

    deployの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.17.0'
 set :application, 'gadget'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:Shaitaxx/gatget.git'
+set :repo_url,  'git@github.com:Shaitaxx/gadget.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
